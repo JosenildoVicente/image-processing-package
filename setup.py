f6473c72a9b28d77d8d3acdb04a0bf742244102e
JosenildoVicente <jva@cin.ufpe.br>
2022-11-07 16:46:39 -0300

    put project code

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="Image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Josenildo Vicente",
+    author_email="jva@cin.ufpe.br",
+    description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/JosenildoVicente/image-processing-package"
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
 )
