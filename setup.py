3c588a459f2e98c028fbb563c16da77f43684b74
JosenildoVicente <jva@cin.ufpe.br>
2022-11-07 17:36:58 -0300

    setup file changes

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="Image_processing",
+    name="Image_processing_Josenildo",
     version="0.0.1",
     author="Josenildo Vicente",
-    author_email="jva@cin.ufpe.br",
+    author_email="josenildovicent@gmail.com",
     description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/JosenildoVicente/image-processing-package"
+    url="https://github.com/JosenildoVicente/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.5',
